844efe71f8f995b9fac18d32d85aa38022b3b246
Pooof Jikly <jiklyp000f@yandex.ru>
2024-04-18 20:13:21 +0300

    УДАЛИЛ ВСЁ

--- FightDND/Fighters/Fighters/IWeapon/NoWeapon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fighters.Models.Weapons;
-
-
-public class NoWeapon : IWeapon
-{
-    public int Damage { get; } = 1;
-    public int Crit { get; } = 1;
-}
--- FightDND/Fighters/Fighters/IWeapon/Sword.cs
+++
@@ -1,7 +0,0 @@
-﻿using Fighters.Models.Weapons;
-
-public class Sword : IWeapon
-{
-    public int Damage { get; } = 10;
-    public int Crit { get; } = 10;
-}
